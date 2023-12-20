d3d000fc4afd4b017af6552742649c11107ca639
Omar Amer <oamer@keybiz.it>
2023-12-20 13:05:37 +0100

    add return to function

--- src/time_series/analysis.py
+++ src/time_series/analysis.py
@@ -1,28 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
-
+import plotly.graph_objs as go
 class Analysis:
     def __init__(self) -> None:
         pass
 
     def check_stationarity(self, df:pd.DataFrame, name:str, window_size:int):
         self.__check_stationarity_approx(df, name)
-        self.__stationarity_rolling_window(df,name, window_size)
-        self.__adfuller_test(df, name)
+        rolling_window_plot =self.__stationarity_rolling_window(df,name, window_size)
+        adfuller_test = self.__adfuller_test(df, name)
+        return rolling_window_plot, adfuller_test
 
     
     
     def __adfuller_test(self, df:pd.DataFrame, name:str):
         time_series_data = df[name]
         result = adfuller(time_series_data)
-        print('ADF Statistic:', result[0])
-        print('p-value:', result[1])
+        ADF_Statistic= result[0]
+        p_value = result[1]
 
         if result[1] <= 0.05:
-            print("Reject the null hypothesis. The time series is likely stationary.")
+            test_result = "Reject the null hypothesis. The time series is likely stationary."
+    
         else:
-            print("Fail to reject the null hypothesis. The time series is likely non-stationary.")
+            test_result="Fail to reject the null hypothesis. The time series is likely non-stationary."
+        return ADF_Statistic, p_value, test_result
 
     
     def __check_stationarity_approx(self, df:pd.DataFrame, name:str):
@@ -32,16 +35,23 @@ class Analysis:
         print(f' \n Mean 1: {mean1}, \n Mean 2: {mean2} \n')
         print(f' \n Variance 1: {var1}, \n Variance 2: {var2}')
     
-    def __stationarity_rolling_window(self, df:pd.DataFrame, name:str, window_size:int ):
-
+    def __stationarity_rolling_window(self, df: pd.DataFrame, name: str, window_size: int):
         df["rolling_mean"] = df[name].rolling(window=window_size).mean()
         df["rolling_standard_deviation"] = df[name].rolling(window=window_size).std()
 
-        plt.plot(df[name], label='Original')
-        plt.plot(df["rolling_mean"], label='Rolling Mean')
-        plt.plot(df["rolling_standard_deviation"], label='Rolling Std')
-        plt.legend()
-        plt.show()
+        # Create a Plotly figure
+        fig = go.Figure()
+
+        # Add traces for original, rolling mean, and rolling standard deviation
+        fig.add_trace(go.Scatter(x=df.index, y=df[name], mode='lines', name='Original'))
+        fig.add_trace(go.Scatter(x=df.index, y=df["rolling_mean"], mode='lines', name='Rolling Mean'))
+        fig.add_trace(go.Scatter(x=df.index, y=df["rolling_standard_deviation"], mode='lines', name='Rolling Std'))
+
+        # Add layout information
+        fig.update_layout(title='Rolling Window Analysis', xaxis_title='Date', yaxis_title=name)
+
+        # Return the Plotly figure
+        return fig
 
     
         
