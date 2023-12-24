f46660d0c56a7cb2cd28df0b5f19cc8b11685ea6
Omar Amer <oamer@keybiz.it>
2023-12-24 14:09:23 +0100

    add docstring and the percentage error

--- src/time_series/validation.py
+++ src/time_series/validation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-
+import numpy as np
 class Validation:
     def __init__(self) -> None:
         pass
@@ -32,10 +32,54 @@ class Validation:
         return fig
     
     def mse_error(self, forecast:pd.DataFrame, test_data:pd.DataFrame, name:str):
+        """
+        Calculate the Mean Squared Error (MSE) between forecasted and actual values.
+
+        Parameters:
+            forecast (pd.DataFrame): DataFrame containing forecasted values and dates.
+            test_data (pd.DataFrame): DataFrame containing actual values and dates for comparison.
+            name (str): The name of the column representing the time series.
+
+        Returns:
+            float: The Mean Squared Error (MSE) between forecasted and actual values.
+
+        """
         return ((forecast[name] - test_data[name])**2).mean()
     
+    
+    
+    def percentage_error(self, forecast: pd.DataFrame, test_data: pd.DataFrame, name: str):
+        """
+        Calculate the Percentage Error between forecasted and actual values.
 
+        Parameters:
+            forecast (pd.DataFrame): DataFrame containing forecasted values and dates.
+            test_data (pd.DataFrame): DataFrame containing actual values and dates for comparison.
+            name (str): The name of the column representing the time series.
+
+        Returns:
+            float: The Percentage Error between forecasted and actual values.
+
+        """
+        n = len(test_data)
+        percentage_error = (1 / n) * ((test_data[name] - forecast[name]) / test_data[name])**2
+        percentage_error = percentage_error.sum() * 100
+        return percentage_error
+    
     def evaluate_forecast_plot(self, forecast:pd.DataFrame, test_data:pd.DataFrame, name:str, y_label:str):
+        """
+        Create a Plotly figure for visual evaluation of forecasted and actual values.
+
+        Parameters:
+            forecast (pd.DataFrame): DataFrame containing forecasted values and dates.
+            test_data (pd.DataFrame): DataFrame containing actual values and dates for comparison.
+            name (str): The name of the column representing the time series.
+            y_label (str): The label for the y-axis.
+
+        Returns:
+            go.Figure: A Plotly figure comparing actual and forecasted values.
+
+        """
         actual_values = test_data[name]
         forecast_values = forecast[name]
         actual_trace = go.Scatter(x=test_data['date_datetime'], y=actual_values, mode='lines', name='Actual')
